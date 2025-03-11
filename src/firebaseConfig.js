c87a0ef028145f2827cc284e2eef6bddc409f79c
İsmail SARITEKE <95230928+sar1teke@users.noreply.github.com>
2025-03-11 10:18:39 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore"; // Bunu ekle
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBn-wfkf1cXrBiltLZeiU0WBrSPzk0Cyoc",
-  authDomain: "sariteke-be177.firebaseapp.com",
-  projectId: "sariteke-be177",
-  storageBucket: "sariteke-be177.firebasestorage.app",
-  messagingSenderId: "720858830497",
-  appId: "1:720858830497:web:979f26772a8656467102e0",
-  measurementId: "G-K8SPV4YBFS"
+  apiKey: "#",
+  authDomain: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#",
+  measurementId: "#"
 };
 
 // Initialize Firebase
